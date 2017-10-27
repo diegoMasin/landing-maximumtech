50343c80182431b3ec4224f0d024948bb5e3eaf3
Diego Masin <diegoifce@gmail.com>
2017-10-27 00:14:47 -0300

    Da serie migração do template: Melhorias no Fique por Dentro.

--- landing_v2/assets/php/subscribe.php
+++ landing_v2/assets/php/subscribe.php
@@ -9,7 +9,7 @@
 
   // choose subscribe form version
 
-  $version = 1; // 1 = save to txt file, 2 = mailchimp (recommend)
+  $version = 2; // 1 = save to txt file, 2 = mailchimp (recommend)
 
 
 
@@ -30,11 +30,11 @@
 
   // mailchimp api key, for more info visit http://admin.mailchimp.com/account/api/
 
-  define('MC_API_KEY', 'YOUR_MAILCHIMP_API_KEY'); // example: define('MC_API_KEY', '1234567890abcdefghti-us10');
+  define('MC_API_KEY', 'YOUR_MAILCHIMP_API_KEY'); // example: define('MC_API_KEY', '96f10bc8458cf6f4fa51c3cac045cd3d-us17');
 
   // mailchimp list id, for more info visit http://admin.mailchimp.com/lists/
 
-  define('MC_LIST_ID', 'YOUR_MAILCHIMP_LIST_ID'); // example: define('MC_LIST_ID', '12345abcde');
+  define('MC_LIST_ID', 'YOUR_MAILCHIMP_LIST_ID'); // example: define('MC_LIST_ID', 'c55a73208d');
 
 
 
@@ -89,13 +89,13 @@
   function fn_saveToFile($form_email) {
     if (defined('FILE_PATH')) {
       if (strpos(file_get_contents(FILE_PATH), $form_email) == true) {
-        throw new Exception('Subscription already exists', 1); // error message
+        throw new Exception('A assinatura já existe.', 1); // error message
       } else {
         file_put_contents(FILE_PATH, date("Y-m-d H:i:s").' - '.$form_email."\n", FILE_APPEND | LOCK_EX); // LOCK_EX available since PHP 5.1.0+
-        throw new Exception('Thank you! You have been successfully subscribed', 0); // success message
+        throw new Exception('Obrigado! Acabamos de enviar-lhe um e-mail de confirmação', 0); // success message
       }
     } else {
-      throw new Exception('An error occurred. Please try again later', 1); // error message
+      throw new Exception('Um erro ocorreu. Por favor, tente novamente mais tarde.', 1); // error message
     }
   }
 
@@ -107,7 +107,7 @@
       $api = new MCAPI(MC_API_KEY);
       if ($api->listSubscribe(MC_LIST_ID, $form_email) !== true) {
         if ($api->errorCode == 214) {
-          throw new Exception('Subscription already exists', 1);
+          throw new Exception('A assinatura já existe.', 1);
         } elseif ($api->errorCode == 104) {
           throw new Exception('Invalid API KEY', 1);
         } elseif ($api->errorCode == 200) {
@@ -116,7 +116,7 @@
           throw new Exception($api->errorMessage, 1);
         }
       } else {
-        throw new Exception('Thank you! We just sent you a confirmation email', 0); // success message
+        throw new Exception('Obrigado! Acabamos de enviar-lhe um e-mail de confirmação.', 0); // success message
       }
     } else {
       throw new Exception('API KEY / LIST ID not defined', 1); // error message
