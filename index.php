204ec4ad77aaf739263ab6ea4a7208be2df60b44
Diego Masin <diegoifce@gmail.com>
2017-11-01 00:27:22 -0200

    Atualizando Google Analytics de acordo com a propriedade cadastrada.

--- index.php
+++ index.php
@@ -456,15 +456,15 @@
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109039390-1"></script>
 <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-109039390-1');
+</script>
 
 </body>
 </html>
