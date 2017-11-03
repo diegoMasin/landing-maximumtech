6d1111dfea8610c97f80dfa21cb483ead6ea8bd9
Diego Masin <diegoifce@gmail.com>
2017-11-03 15:39:09 -0300

    melhorando e salientando mais no contato.

--- index.php
+++ index.php
@@ -357,6 +357,9 @@
                                 </div>
                                 <div class="col-xs-12 col-md-5 m-t-30-sm-max">
                                     <div class="col-inner">
+                                        <div class="iconbox">
+                                            Está com problemas ou precisa de solução? Fale conosco! Vamos análisar e estudar a viabilidade de uma soluções para você.
+                                        </div>
                                         <div class="iconbox _left">
                                             <div class="iconbox-icon">
                                                 <i class="icon-tablet"></i>
